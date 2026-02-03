349ae9e4411f92a12e4a7c58b026ba40e2e0a60e
Soleeping <solp-wm24@student.tarc.edu.my>
2026-02-03 17:15:17 +0800

    88

---
+++ app-files/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-55055006922_00547b81f2_o",
+      "name": "55055006922_00547b81f2_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.40910857917700305,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5738756965245573,
+          "pitch": -0.12871095025545998,
+          "rotation": 0,
+          "target": "1-55044769427_df7cc53951_o"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-55044769427_df7cc53951_o",
+      "name": "55044769427_df7cc53951_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.669248137940162,
+          "pitch": 0.1648292029744418,
+          "rotation": 0,
+          "target": "0-55055006922_00547b81f2_o"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
